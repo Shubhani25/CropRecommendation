6d771b63a146d7a64badd1e1e7aae829ded8f05b
Shubhani25 <shubhanimulay25@gmail.com>
2023-05-01 14:06:52 +0530

    Added Files

---
+++ index.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html>
+<?php include ('header.php');  ?>
+
+  <body class="bg-white" id="top">
+  
+<?php include ('nav.php');  ?>
+
+<div class="wrapper" >
+
+    <div class="wrapper">
+      <header class="jumbotron bg-gradient-warning">
+        <div class="container">
+          <div class="row row-header">
+            <div class="col-12 col-sm-7">
+              <h1 class="text-white">Crop Management System</h1>
+              <p class="text-white">
+               A True Farmer's Friend.
+              </p>
+              <div class="cg">
+                <div class="card card-body bg-gradient-success">
+                  <blockquote cite="blockquote">
+                    <h6 class="mb-0 text-dark">
+                      <em
+                        ><b
+                          > “Farming looks mighty easy when your plow is a pencil, and you're a thousand miles from the corn field..”</b
+                        ></em
+                      >
+                    </h6>
+                    <br />
+
+                    <footer class="blockquote-footer vg text-dark">
+                      DWIGHT D. EISENHOWER
+                    </footer>
+                  </blockquote>
+                </div>
+              </div>
+            </div>
+            <div class="col-12 col-sm-3 offset-sm-2 align-self-center">
+              <img src="assets/img/plant-bulb.png" class="img-fluid" alt="" />
+            </div>
+          </div>
+        </div>
+      </header>
+<!-- Page Content -->
+
+<!-- ======================================================================================================================================== -->
+
+    <div class="section features-6 text-dark bg-white" id="services">
+      <div class="container ">
+
+        <div class="row">
+                <div class="col-md-8 mx-auto text-center">
+                    <span class="badge badge-primary badge-pill mb-3">Insight</span>
+                    <h3 class="display-3 ">Features</h3>
+                </div>
+            </div>
+			<br>
+			
+        <div class="row align-items-center">
+		
+          <div class="col-lg-6">
+            <div class="info info-horizontal info-hover-success">
+              <div class="description pl-4">
+                <h3 class="title" >Farmers</h3>
+           <p class=" ">Farmers can get recommendations for crop and fertilizer and even 
+            predictions for crop, yield and rainfall. All these are implemented using ML with proper dataset. </p>
+                        
+              </div>
+            </div>
+
+          </div>
+		  
+		  
+          <div class="col-lg-6 col-10 mx-md-auto d-none d-md-block">
+            <img class="ml-lg-5  pull-right" src="assets/img/agri.png" width="100%">
+          </div>
+        </div>
+		
+		
+
+      </div>
+    </div>
+
+
+     
+<!-- ======================================================================================================================================== -->
+
+      <div class="section features-2 text-dark bg-white" id="features"> 
+        <div class="container"> 
+          <div class="row align-items-center"> 
+            <div class="col-lg-5 col-md-8 mr-auto text-left"> 
+              <div class="pr-md-5"> 
+                <div class="icon icon-lg icon-shape icon-shape-primary shadow rounded-circle mb-5"> <i class="ni ni-favourite-28"> </i></div>
+                <h3 class="display-3 text-justify">Features</h3>
+                <p>The time is now for the next step in farming. We bring you the future of farming along with great tools for asisting the farmers.</p>
+                <ul class="list-unstyled mt-5"> 
+                  <li class="py-2"> 
+                    <div class="d-flex align-items-center"> 
+                      <div>
+                        <div class="badge badge-circle badge-primary mr-3"> <i class="ni ni-settings-gear-65"> </i></div>
+                      </div>
+                      <div>
+                        <h6 class="mb-0">Crop Recommendation</h6>
+                      </div>
+                    </div>
+                  </li>
+                 
+                  <li class="py-2"> 
+                    <div class="d-flex align-items-center"> 
+                      <div>
+                        <div class="badge badge-circle badge-primary mr-3"> <i class="ni ni-settings-gear-65"> </i></div>
+                      </div>
+                      <div>
+                        <h6 class="mb-0">yield Prediction</h6>
+                      </div>
+                    </div>
+                  </li>
+                  
+                </ul>
+              </div>
+            </div>
+			
+
+		  
+            <div class="col-lg-7 col-md-12 pl-md-0"> 
+ <img class="img-fluid ml-lg-5" src="assets/img/features.png" width="100%">
+ </div>
+			
+			
+          </div>
+        </div>
+        <span > </span>
+      </div>
+     
+	<!-- ======================================================================================================================================== -->
+ 
+	 
+
+ <div class="section features-6 text-dark bg-white" id="tech">
+        <div class="container-fluid shado">
+
+            
+
+            <div class="row text-lg-center align-self-center">
+
+</div>
+
+<div class="row text-center ">            
+           
+</div>
+
+
+        </div>
+    </div>
+
+<?php require("footer.php");?> 
+
+
+</body>
+
+</html>
